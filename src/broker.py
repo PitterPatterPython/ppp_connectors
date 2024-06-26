17d4ab7ad937758cb97bf814f9c22e73b61110b7
Rob D'Aveta <rob.daveta@gmail.com>
2024-06-26 14:21:47 -0400

    updated prototype pattern, added types, updated .env.sample

--- src/broker.py
+++ src/broker.py
@@ -1,16 +1,60 @@
 import niquests as requests
+from typing import Callable, Dict, Any
 from dotenv import dotenv_values
 
-env_config = dotenv_values("../.env")
+
+env_config: Dict = dotenv_values('../.env')
+
+if not env_config:
+    raise FileNotFoundError('The .env file doesn\'t exist or is empty. Did you copy the'
+                            '.env.sample file to .env and set your values?')
 
 
-def make_request(method, url, headers, payload):
-    if not env_config:
-        raise FileNotFoundError("The .env file doesn't exist or is empty. Did you copy the"
-                                ".env.sample file to .env and set your values?")
+def make_request(
+    method: str,
+    url: str,
+    headers: Dict[str, str] = None,
+    params: Dict[str, Any] = None,
+    data: Dict[str, Any] = None,
+    json: Dict[str, Any] = None
+) -> requests.Response:
+    """_summary_
 
-    print(env_config)
-    print(method)
-    print(url)
-    print(headers)
-    print(payload)
+    Args:
+        method (str): _description_
+        url (str): _description_
+        headers (Dict[str, str], optional): _description_. Defaults to None.
+        params (Dict[str, Any], optional): _description_. Defaults to None.
+        data (Dict[str, Any], optional): _description_. Defaults to None.
+        json (Dict[str, Any], optional): _description_. Defaults to None.
+
+    Raises:
+        FileNotFoundError: _description_
+
+    Returns:
+        requests.Response: _description_
+    """
+
+    proxies: Dict = {
+        'http_proxy': env_config['HTTP_PROXY'],
+        'https_proxy': env_config['HTTPS_PROXY']
+    }
+
+    verify: str = False if env_config['VERIFY_SSL'].lower() == "false" else True
+    if verify is False:
+        import urllib3
+        urllib3.disable_warnings()
+
+    method_map: Dict[str, Callable] = {
+        'GET': requests.get,
+        'POST': requests.post,
+        'PUT': requests.put,
+        'DELETE': requests.delete,
+        'PATCH': requests.patch
+    }
+
+    request_func = method_map.get(method.upper())
+    if not request_func:
+        raise ValueError(f'Unsupported HTTP method: {method}')
+
+    return request_func(url, headers=headers, params=params, data=data, json=json, proxies=proxies, verify=verify)
--- src/spycloud.py
+++ src/spycloud.py
@@ -1,14 +1,30 @@
 from broker import make_request
+from niquests import Response
+from typing import Dict, Any
+from dotenv import dotenv_values
 
 
-def sip_cookie_domains(cookie_domains, **kwargs):
-    method = "get"
-    url = "https://test"
-    headers = {}
-    payload = {}
+env_config: Dict = dotenv_values('../.env')
 
-    make_request(method=method, url=url, headers=headers, payload=payload)
+if not env_config:
+    raise FileNotFoundError('The .env file doesn\'t exist or is empty. Did you copy the'
+                            '.env.sample file to .env and set your values?')
 
 
-if __name__ == "__main__":
-    sip_cookie_domains("test")
+def sip_cookie_domains(cookie_domains: str, **kwargs: Dict[str, Any]) -> Dict:
+    method: str = 'get'
+    url: str = f'https://api.spycloud.io/sip-v1/breach/data/cookie-domains/{cookie_domains}'
+    headers: Dict = {
+        'accept': 'application/json',
+        'x-api-key': env_config['SPYCLOUD_API_SIP_KEY']
+    }
+    params: Dict = dict(kwargs)
+
+    result: Response = make_request(method=method, url=url, headers=headers, params=params)
+
+    print(result.status_code)
+    print(result.content)
+
+
+if __name__ == '__main__':
+    sip_cookie_domains('wellsfargo.com', since="2024-04-24")
